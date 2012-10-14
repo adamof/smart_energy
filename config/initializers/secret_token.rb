79040a277a9f87a9bd40a6456dd6559c03c14f18
badjo <stefan.adamof@gmail.com>
2012-10-14 16:03:16 +0100

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartEnergy::Application.config.secret_token = '4d7fa32241f031468a471097ae98e9e560fe7bde94eb0077652539d8d9d6bb5f411736f3754f5b897dabdaf0bcf9cbd28f06e59d0c4a980a7801e0e7043002dd'
