cb83e60dc903fe9fa6089188d87d671c3ec5f833
badjo <stefan.adamof@gmail.com>
2012-10-17 00:17:00 +0100

    user setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  def home
+  end
+end
---
+++ db/migrate/20121016230634_create_user_table.rb
@@ -0,0 +1,11 @@
+class CreateUserTable < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
