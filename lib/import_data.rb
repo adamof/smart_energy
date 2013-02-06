8729fa9a24799a46b89e601fa51f23653cdd2674
badjo <stefan.adamof@gmail.com>
2013-02-06 18:38:32 +0000

    adding data for last year

--- lib/import_data.rb
+++ lib/import_data.rb
@@ -5,7 +5,7 @@ module ImportData
   end
   def self.init(n=nil)
     self.delete_data
-    self.import_usage("readings.xls", n)
+    self.import_usage("readings_2010.xls", n)
   end
   def self.import_usage(file, n_households=0)
     s = Excel.new(file)
@@ -15,22 +15,22 @@ module ImportData
       amount     = s.cell(line,'B')
       date      = s.cell(line,'C')
       puts "#{line}: #{household}, #{amount}, #{date}"
-      household = Household.find_or_create_by_name(household)
-      if Household.count==n_households+1
-        household.delete
-        break
-      end
+      household = Household.find_or_create_by_name(household.to_s)
+      # if Household.count==n_households+1
+      #   household.delete
+      #   break
+      # end
       distribution = (rand(3) + 4).to_f
-      this = Record.create household: household, 
+      this = PowerRecord.create household: household, 
                               amount: (10 - distribution)*amount/10,
                               period_end: date.asctime
       if last_record and last_record.household == household
         if this.period_end - last_record.period_end > 3600
-          last_record = Record.create household: household, 
+          last_record = PowerRecord.create household: household, 
                               amount: (amount + last_record.amount)/2.0,
                               period_end: date-1.hour
         end
-        Record.create household: household, 
+        PowerRecord.create household: household, 
                             amount: distribution*amount/10,
                             period_end: date-30.minutes
       end
@@ -41,14 +41,14 @@ module ImportData
     
   end
   def self.import_intensities
-    CarbonRecord.delete_all
-    CSV.foreach("intensities.2011.csv", headers: false) do |r|
+    # CarbonRecord.delete_all
+    CSV.foreach("intensities.2010.csv", headers: false) do |r|
       CarbonRecord.create!( period_end: DateTime.strptime(r[0], "%Y/%m/%d %H:%M"),
                             amount: r[1] )
     end
   end
   def self.refresh_intensities
-    Record.each do |r|
+    PowerRecord.all.each do |r|
       r.save!
     end
   end
