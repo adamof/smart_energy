f01fec1e43a649cde8aa8b43eba9748e6249b560
badjo <stefan.adamof@gmail.com>
2013-02-17 19:58:45 +0000

    routes for survey

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ SmartEnergy::Application.routes.draw do
     end
   end
 
+  match 'survey' => 'households#chart_drill', :defaults => { :id => Household.last.id }
+
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
   devise_for :users
